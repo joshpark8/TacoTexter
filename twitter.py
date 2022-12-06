2a80ef6428a28085de9ed84b1bf07a3d2e6aa73a
josh park <wljoshpark@gmail.com>
2022-12-06 17:05:04 -0500

    change comparison in main, optimize for fewer API calls

--- twitter.py
+++ twitter.py
@@ -3,6 +3,10 @@ import requests
 import subprocess
 import time
 import sys
+import json
+from datetime import datetime
+
+
 
 from os import environ
 
@@ -14,48 +18,41 @@ client_secret = environ.get("TWITTER_OAUTH_CLIENT_SECRET")
 
 # start_time,end_time,since_id,until_id,max_results,next_token,
 #   expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': 'from:ChipotleTweets','max_results': '10'}
+query_params = {'query': 'from:joshparktestacc','max_results': '10'}
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2RecentSearchPython"
     return r
 
-def connect_to_endpoint(url, query_params):
-    response = requests.get(url, auth=bearer_oauth, params=query_params)
+def get_most_recent_tweet():
+    response = requests.get(f'{base_url}tweets/search/recent', auth=bearer_oauth, params=query_params)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
-    return response.json()
-
-def connect_to_endpoint2(url):
-    response = requests.request('get', url, auth=bearer_oauth)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    return response.json()
-
-def get_most_recent_ID():
-    json_response = connect_to_endpoint(f'{base_url}tweets/search/recent', query_params)
-    ids = []
-    return json_response['data'][0]['id']
-
-def get_tweet():
-    tweets = []
-    id = get_most_recent_ID()
-    r = connect_to_endpoint2(f'{base_url}tweets?ids={id}')['data'][0]
-    return (id, r['text'])
+    
+    # text contents of most recent tweet
+    r = response.json()
+    return r['data'][0]['text']
 
 if __name__ == "__main__":
-    used_ids = []
+    last_tweet = None
+    current_time = datetime.now().strftime('%H:%M:%S')
+    print(f'starting at {current_time}')
     while True:
         time.sleep(1)
-        id, tweet = get_tweet()
-        if (id not in used_ids):
+        tweet = get_most_recent_tweet()
+        if tweet != last_tweet:
             phrase = re.search("\w+\d+ to \d+", str(tweet))
             if phrase:
-                split_phrase = phrase.group().split()
-                term = split_phrase[0]
+                current_time = datetime.now().strftime('%H:%M:%S')
+                print(f'got tweet at {current_time}')
+
+                term = phrase.group().split()[0]
                 subprocess.run(['osascript', 'text.applescript', term]) 
-                print(f'\nsent {term} to 888222')
+                
+                current_time = datetime.now().strftime('%H:%M:%S')
+                print(f'\nsent {term} to 888222 at {current_time}')
+
+            last_tweet = tweet
         print('.', end='')
         sys.stdout.flush()
-        used_ids.append(id)
