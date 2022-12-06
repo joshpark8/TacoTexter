5186ef59912b7f362561f99bc6986046300753ea
josh park <wljoshpark@gmail.com>
2022-12-06 17:13:18 -0500

    update twitter.py to get creation time of tweet

--- twitter.py
+++ twitter.py
@@ -18,7 +18,7 @@ client_secret = environ.get("TWITTER_OAUTH_CLIENT_SECRET")
 
 # start_time,end_time,since_id,until_id,max_results,next_token,
 #   expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': 'from:joshparktestacc','max_results': '10'}
+query_params = {'query': 'from:joshparktestacc','tweet.fields': 'created_at', 'max_results': '10'}
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
@@ -32,7 +32,7 @@ def get_most_recent_tweet():
     
     # text contents of most recent tweet
     r = response.json()
-    return r['data'][0]['text']
+    return (r['data'][0]['text'], r['data'][0]['created_at'])
 
 if __name__ == "__main__":
     last_tweet = None
@@ -40,12 +40,12 @@ if __name__ == "__main__":
     print(f'starting at {current_time}')
     while True:
         time.sleep(1)
-        tweet = get_most_recent_tweet()
+        tweet, creation_time = get_most_recent_tweet()
         if tweet != last_tweet:
             phrase = re.search("\w+\d+ to \d+", str(tweet))
             if phrase:
                 current_time = datetime.now().strftime('%H:%M:%S')
-                print(f'\ngot tweet at {current_time}')
+                print(f'\ntweet created at {creation_time}, recieved at {current_time}')
 
                 term = phrase.group().split()[0]
                 subprocess.run(['osascript', 'text.applescript', term, '888222']) 
